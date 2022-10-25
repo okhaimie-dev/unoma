eaae525c9f323a903f685ca001fd9bd93bdc812b
Victor Omorogbe <victoromorogbe69@gmail.com>
2022-10-25 19:39:01 +0100

    suprememods

--- src/unoma-app/components/VericalCard.tsx
+++ src/unoma-app/components/VericalCard.tsx
@@ -1,7 +1,17 @@
-export default function VericalCard() {
+import Image from "next/image";
+
+interface Props {
+    thumbnail: string
+}
+
+export default function VericalCard({ thumbnail }: Props) {
   return (
     <div>
-      
+      <div>
+        <Image src={thumbnail} width={185} height={150} />
+      </div>
+      <h3 className="h3 text-white">Podcast Title</h3>
+      <p className="p text-white">10 episodes</p>
     </div>
   )
 }
--- src/unoma-app/pages/index.tsx
+++ src/unoma-app/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Nav2 from '../components/Nav2'
 import SideNav from '../components/SideNav'
 import TopNav from '../components/TopNav'
+import VericalCard from '../components/VericalCard'
 
 const Home: NextPage = () => {
   return (
@@ -14,9 +15,19 @@ const Home: NextPage = () => {
       </Head>
       <main className='flex bg-black h-screen'>
         <div className='w-72'><SideNav page='home' /></div>
-        <div className='flex-1 flex flex-col overflow-x-scroll scrollbar-none'>
+        <div className='flex-1 flex flex-col overflow-x-scroll  overflow-y-scroll scrollbar-none'>
           <TopNav />
           <Nav2 />
+          <div className='px-5 grid grid-cols-4 space-y-4 border-b-2 border-b-purple py-5'>
+            <VericalCard thumbnail="/card1.png" />
+            <VericalCard thumbnail="/card2.png" />
+            <VericalCard thumbnail="/card3.png" />
+            <VericalCard thumbnail="/card4.png" />
+            <VericalCard thumbnail="/card5.png" />
+            <VericalCard thumbnail="/card6.png" />
+            <VericalCard thumbnail="/card7.png" />
+            <VericalCard thumbnail="/card8.png" />
+          </div>
         </div>
       </main>
     </div>
