c2077b1aa0eaeb3e3e55d4866bf40da043010582
Victor Omorogbe <victoromorogbe69@gmail.com>
2022-10-27 12:23:00 +0100

    suprememods

--- src/unoma-app/pages/index.tsx
+++ src/unoma-app/pages/index.tsx
@@ -11,16 +11,16 @@ const Home: NextPage = () => {
     <div>
       <Head>
         <title>Unoma</title>
-        <meta name="description" content="Unoma homepage" />
+        <meta name="description" content="Unoma categories" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className='relative md:flex bg-black h-screen scrollbar-thin scrollbar-thumb-[#008BEE]'>
-        <div className='relative z-20'><SideNav page='home' /></div>
+        <div className='relative z-20'><SideNav page='categories' /></div>
         <div className='flex-1 w-full md:pl-72'>
           <div className='flex flex-col overflow-y-scroll scrollbar-none'>
             <TopNav />
             <Nav2 />
-            <div className='px-5 grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-5 2xl:grid-cols-6 gap-4 border-b-2 border-b-purple py-5'>
+            <div className='grid grid-cols-2 gap-4 px-5 py-5 border-b-2 sm:grid-cols-3 xl:grid-cols-5 2xl:grid-cols-6 border-b-purple'>
               <VericalCard thumbnail="/card1.png" />
               <VericalCard thumbnail="/card2.png" />
               <VericalCard thumbnail="/card3.png" />
@@ -35,7 +35,7 @@ const Home: NextPage = () => {
               <VericalCard thumbnail="/card8.png" />
             </div>
             <div className='p-5'>
-              <h2 className='h2 text-white'>Top 5 Podcasters</h2>
+              <h2 className='text-white h2'>Top 5 Podcasters</h2>
               <div className='flex space-x-5 py-2.5 overflow-x-scroll scrollbar-thin scrollbar-thumb-[#008BEE]'>
                 <HorizontalCard thumbnail='/podcaster1.png' />
                 <HorizontalCard thumbnail='/podcaster2.png' />
@@ -55,10 +55,10 @@ export default Home
 
 /**
  * 
- * <div className='flex-1 flex flex-col overflow-x-scroll overflow-y-scroll scrollbar-none'>
+ * <div className='flex flex-col flex-1 overflow-x-scroll overflow-y-scroll scrollbar-none'>
           <TopNav />
           <Nav2 />
-          <div className='px-5 grid grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 border-b-2 border-b-purple py-5'>
+          <div className='grid grid-cols-3 gap-4 px-5 py-5 border-b-2 xl:grid-cols-4 2xl:grid-cols-5 border-b-purple'>
             <VericalCard thumbnail="/card1.png" />
             <VericalCard thumbnail="/card2.png" />
             <VericalCard thumbnail="/card3.png" />
@@ -69,7 +69,7 @@ export default Home
             <VericalCard thumbnail="/card8.png" />
           </div>
           <div className='p-5'>
-            <h2 className='h2 text-white'>Top 5 Podcaster</h2>
+            <h2 className='text-white h2'>Top 5 Podcaster</h2>
             <div className='flex space-x-5 py-2.5 overflow-x-scroll scrollbar-thin scrollbar-thumb-[#008BEE]'>
               <HorizontalCard thumbnail='/podcaster1.png' />
               <HorizontalCard thumbnail='/podcaster2.png' />
